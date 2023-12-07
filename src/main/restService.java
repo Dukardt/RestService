c9f188582b9b96a5246a88b3735b486976417b64
Valery Dukardt <valeradukardt@mail.ru>
2023-12-07 18:50:14 +0700

    Initial commit

---
+++ src/main/restService.java
@@ -0,0 +1,20 @@
+package ru.netology.services;
+
+public class restService {
+    public int calculate(int income, int expenses, int threshold) {
+        int balance = 0;
+        int count = 0;
+
+        for (int i = 0; i < 12; i++) {
+            if (balance >= threshold) {
+                balance -= expenses;
+                balance /= 3;
+                count++;
+            } else {
+                balance += income;
+                balance -= expenses;
+            }
+        }
+        return count;
+    }
+}
